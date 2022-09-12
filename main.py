e79d8f2e6966578b1a39e708c27e7f1967e45a08
Jair Semblantes <jair_semblantes@hotmail.com>
2022-09-11 20:42:08 -0500

    cors

--- main.py
+++ main.py
@@ -22,8 +22,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["POST", "GET"],
     allow_headers=["*"],
     max_age=3600,
