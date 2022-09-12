dc76895e788b51c0edcf59ccedfc82a4bf06aff6
Jair Semblantes <jair_semblantes@hotmail.com>
2022-09-11 20:29:30 -0500

    cors

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:3000",
-    "*",
-    "https://ecg-plus.herokuapp.com"
+    "https://ecg-plus.herokuapp.com",
+    "http://ecg-plus.herokuapp.com"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
